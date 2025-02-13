58ce0c65d3540e573a8589ec5984f4eaf3336077
Mili <santiagoogle@hotmail.com>
2025-02-13 19:33:31 +0000

    fix: correct typo and import format

--- pumpkin/src/world/mod.rs
+++ pumpkin/src/world/mod.rs
@@ -48,7 +48,8 @@ use pumpkin_world::{
     },
     coordinates::ChunkRelativeBlockCoordinates,
 };
-use rand::{thread_rng, Rng};use rayon::{iter::ParallelIterator, slice::ParallelSlice};
+use rand::{thread_rng, Rng};
+use rayon::{iter::ParallelIterator, slice::ParallelSlice};
 use scoreboard::Scoreboard;
 use thiserror::Error;
 use time::LevelTime;
@@ -993,7 +994,7 @@ impl World {
         rayon::spawn(move || {
             // Split the chunks into 64 chunks per request
             // this way simulate an in order request
-            // which improbes login experience
+            // which improves login experience
             chunks.par_chunks(64).for_each(|chunks| {
                 level.fetch_chunks(chunks, sender.clone(), &rt);
             });
