f11e106891b39692fa90ea5445f4017a18c5dcd7
kralverde <github@email.kralverde.dev>
2025-03-22 16:15:22 -1000

    tweak tests

--- pumpkin-world/src/biome/multi_noise.rs
+++ pumpkin-world/src/biome/multi_noise.rs
@@ -2,7 +2,7 @@ use pumpkin_data::chunk::Biome;
 use serde::{Deserialize, Serialize};
 
 pub fn to_long(float: f32) -> i64 {
-    (float * 10000.0) as i64
+    (float * 10000f32) as i64
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
@@ -29,17 +29,21 @@ impl NoiseValuePoint {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq)]
 pub struct ParameterRange {
     min: i64,
     max: i64,
 }
 
 impl ParameterRange {
-    fn get_distance(&self, noise: i64) -> i64 {
-        let l = noise - self.max;
-        let m = self.min - noise;
-        if l > 0 { l } else { m.max(0) }
+    fn calc_distance(&self, noise: i64) -> i64 {
+        if noise > self.max {
+            noise - self.max
+        } else if noise < self.min {
+            self.min - noise
+        } else {
+            0
+        }
     }
 
     pub fn combine(&self, other: &Self) -> Self {
@@ -50,7 +54,7 @@ impl ParameterRange {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq)]
 #[serde(tag = "_type", rename_all = "lowercase")]
 pub enum BiomeTree {
     Leaf {
@@ -98,7 +102,7 @@ impl BiomeTree {
                     let node_distance = node.get_squared_distance(point_list);
                     if distance > node_distance {
                         let node2 = node.get_resulting_node(point_list, best_node);
-                        let node2_distance = if std::ptr::eq(node, node2) {
+                        let node2_distance = if node == node2 {
                             node_distance
                         } else {
                             node2.get_squared_distance(point_list)
@@ -116,7 +120,7 @@ impl BiomeTree {
         }
     }
 
-    fn get_squared_distance(&self, other: &[i64; 7]) -> i64 {
+    fn get_squared_distance(&self, point_list: &[i64; 7]) -> i64 {
         let parameters = match self {
             Self::Leaf { parameters, .. } => parameters,
             Self::Branch { parameters, .. } => parameters,
@@ -124,9 +128,9 @@ impl BiomeTree {
 
         parameters
             .iter()
-            .zip(other)
+            .zip(point_list)
             .map(|(bound, value)| {
-                let distance = bound.get_distance(*value);
+                let distance = bound.calc_distance(*value);
                 distance * distance
             })
             .sum()
@@ -135,15 +139,21 @@ impl BiomeTree {
 
 #[cfg(test)]
 mod test {
-    use pumpkin_util::math::vector2::Vector2;
+    use pumpkin_data::chunk::Biome;
+    use pumpkin_util::math::{vector2::Vector2, vector3::Vector3};
 
     use crate::{
         GENERATION_SETTINGS, GeneratorSetting, GlobalProtoNoiseRouter, GlobalRandomConfig,
-        NOISE_ROUTER_ASTS, ProtoChunk, read_data_from_file,
+        NOISE_ROUTER_ASTS, ProtoChunk,
+        biome::{BiomeSupplier, MultiNoiseBiomeSupplier},
+        generation::noise_router::multi_noise_sampler::{
+            MultiNoiseSampler, MultiNoiseSamplerBuilderOptions,
+        },
+        read_data_from_file,
     };
 
     #[test]
-    fn test_sample_chunk() {
+    fn test_sample_value() {
         type PosToPoint = (i32, i32, i32, i64, i64, i64, i64, i64, i64);
         let expected_data: Vec<PosToPoint> =
             read_data_from_file!("../../assets/multi_noise_sample_no_blend_no_beard_0_0_0.json");
@@ -168,6 +178,38 @@ mod test {
             assert_eq!(point.erosion, ero);
             assert_eq!(point.depth, dep);
             assert_eq!(point.weirdness, wei);
+        }
+    }
+
+    #[test]
+    fn test_sample_multinoise_biome() {
+        let expected_data: Vec<(i32, i32, i32, u16)> =
+            read_data_from_file!("../../assets/multi_noise_biome_source.json");
+
+        let seed = 0;
+        let random_config = GlobalRandomConfig::new(seed, false);
+        let noise_router =
+            GlobalProtoNoiseRouter::generate(&NOISE_ROUTER_ASTS.overworld, &random_config);
+
+        let mut sampler = MultiNoiseSampler::generate(
+            &noise_router,
+            &MultiNoiseSamplerBuilderOptions::new(0, 0, 4),
+        );
+
+        for (x, y, z, biome_id) in expected_data {
+            let global_biome_pos = Vector3::new(x, y, z);
+            let calculated_biome = MultiNoiseBiomeSupplier::biome(&global_biome_pos, &mut sampler);
+
+            assert_eq!(
+                biome_id,
+                calculated_biome.to_id(),
+                "Expected {:?} was {:?} at {},{},{}",
+                Biome::from_id(biome_id),
+                calculated_biome,
+                x,
+                y,
+                z
+            );
         }
     }
 }
